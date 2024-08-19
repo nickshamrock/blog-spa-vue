461a731b68c196b31315176c473b8261e69de683
Nikolay <nickshamrock1997@gmail.com>
2024-08-19 16:56:24 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="test mt-[200px]">Запуск проекта</p>
+  <p></p>
+</template>
+
+<style lang="sass" scoped>
+
+.test
+  color: black
+  text-decoration: underline
+  font-size: 50px
+</style>
