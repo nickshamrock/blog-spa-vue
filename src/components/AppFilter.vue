b2570d3f1a6f25e39c3e0712848469f02ad7f7d9
Nikolay <nickshamrock1997@gmail.com>
2024-09-12 14:16:36 +0300

    refactoring and fixing bags in the AppFilter.vue component

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -2,8 +2,8 @@
 import { ref, watch } from 'vue'
 import store from '@/store'
 
-const showFilters = ref<boolean>(false)
-const searchQuery = ref<string>(store.searchQuery)
+const showFilters = ref(false)
+const searchQuery = ref(store.searchQuery)
 const filters = ref<string[]>([
   'Город',
   'Природа',
@@ -14,132 +14,99 @@ const filters = ref<string[]>([
   'Архитектура',
   'Искусство'
 ])
-const selectedFilters = ref<string[]>(store.selectedFilters)
-
-// Функция для проверки, является ли фильтр активным
-const isActiveFilter = (filter: string): boolean => {
-  return selectedFilters.value.includes(filter.replace(' +', ''))
-}
-
-// Функция для переключения фильтра
-const toggleFilterSelection = (filter: string): void => {
-  const cleanFilter = filter.replace(' +', '') // Удаление знака "+"
-  const index = selectedFilters.value.indexOf(cleanFilter)
-  if (index > -1) {
-    selectedFilters.value.splice(index, 1)
-    resetIcon(cleanFilter) //вызываем функцию для смены иконки на плюс сразу после деактивации
-  } else {
-    selectedFilters.value.push(cleanFilter)
-    showCrossIcon(cleanFilter) //вызываем функцию для смены иконки
-  }
-  store.selectedFilters = selectedFilters.value // Обновляем хранилище
-}
-
-// Функция для очистки фильтров и поиска
-const clearFilters = (): void => {
-  selectedFilters.value = []
-  store.selectedFilters = []
-  searchQuery.value = ''
-  store.searchQuery = ''
-  resetAllIcons() //вызываем функцию, чтобы добавить плюс для всех кнопок
-}
-
-// Слежение за изменениями в searchQuery
-watch(searchQuery, (newValue) => {
-  store.searchQuery = newValue
-})
-
-// Слежение за изменениями в selectedFilters
-watch(selectedFilters, (newValue) => {
-  store.selectedFilters = newValue
-})
-
-// Функция для переключения видимости фильтров
-const toggleFilter = (): void => {
-  showFilters.value = !showFilters.value
-}
-
-// Функция для очистки строки поиска
-const clearSearch = (): void => {
-  searchQuery.value = ''
-}
-
-// Логика изменения значка на кнопках-фильтрах
+const selectedFilters = ref(store.selectedFilters)
 const currentIcons = ref<Record<string, string>>({})
 
 filters.value.forEach((filter) => {
   currentIcons.value[filter] = '/svg/plus.svg'
 })
 
-const showCrossIcon = (filter: string): void => {
-  if (isActiveFilter(filter)) {
-    currentIcons.value[filter] = '/svg/cross.svg'
-  }
+const isActiveFilter = (filter: string): boolean => {
+  return selectedFilters.value.includes(filter)
 }
 
-const resetIcon = (filter: string): void => {
-  if (isActiveFilter(filter)) {
-    currentIcons.value[filter] = '/svg/check.svg'
+const toggleFilterSelection = (filter: string): void => {
+  const index = selectedFilters.value.indexOf(filter)
+
+  if (index > -1) {
+    selectedFilters.value.splice(index, 1)
   } else {
-    currentIcons.value[filter] = '/svg/plus.svg'
+    selectedFilters.value.push(filter)
   }
+
+  updateIcon(filter)
+  store.selectedFilters = selectedFilters.value
 }
 
-const getIcon = (filter: string): string => {
-  return currentIcons.value[filter] || '/svg/plus.svg'
+const clearFilters = (): void => {
+  selectedFilters.value = []
+  store.selectedFilters = []
+  searchQuery.value = ''
+  store.searchQuery = ''
+  resetAllIcons()
 }
 
-//функция возврата плюсиков на фильтры
+const toggleFilter = (): void => {
+  showFilters.value = !showFilters.value
+}
+
+const clearSearch = (): void => {
+  searchQuery.value = ''
+}
+
+const updateIcon = (filter: string): void => {
+  currentIcons.value[filter] = isActiveFilter(filter) ? '/svg/cross.svg' : '/svg/plus.svg'
+}
+
 const resetAllIcons = (): void => {
   filters.value.forEach((filter) => {
-    currentIcons.value[filter] = 'svg/plus.svg'
+    currentIcons.value[filter] = '/svg/plus.svg'
   })
 }
 
-const inputField = ref<HTMLInputElement | null>(null)
+watch([searchQuery, selectedFilters], ([newSearchQuery, newSelectedFilters]) => {
+  store.searchQuery = newSearchQuery
+  store.selectedFilters = newSelectedFilters
+})
 </script>
 
 <template>
-  <div class="app-filter-container w-full bg-white">
-    <div class="app-filter-wrapper px-[95px] py-5" v-auto-animate>
-      <div class="app-filter-header flex items-center gap-10">
+  <div class="w-full bg-white">
+    <div class="app-filter-wrapper py-5" v-auto-animate>
+      <div class="flex items-center gap-10">
         <h1 class="app-filter-title text-[32px] font-bold leading-8 text-[#181C32]">Блог</h1>
         <div class="app-filter-input-container relative flex items-center">
-          <img
-            src="/svg/search-icon.svg"
-            class="absolute left-3 block"
-            width="14px"
-            height="14px"
-          />
+          <img src="/svg/search-icon.svg" class="absolute left-3" width="14px" height="14px" />
           <input
             v-model="searchQuery"
-            class="input-filter h-[40px] w-full rounded-md bg-[#F9F9F9] px-[34px] py-[13px]"
+            class="input-filter h-[40px] w-full rounded-md bg-[#F9F9F9] px-[34px] py-[13px] text-sm"
             type="text"
             placeholder="Поиск"
-            name="input"
-            ref="inputField"
+            name="search-input"
           />
           <span
             v-if="searchQuery"
             @click="clearSearch"
-            class="absolute right-[10px] inline-block cursor-pointer text-[10px] text-gray-500"
+            class="absolute right-[10px] cursor-pointer text-[10px] text-gray-500"
           >
             &#10005;
           </span>
         </div>
-        <div class="ml-auto">
+        <div class="ml-auto flex">
           <button
             v-if="selectedFilters.length > 0"
             @click="clearFilters"
-            class="mr-[10px] text-sm font-medium leading-[14px] text-[#2884EF]"
+            class="mr-[10px] text-sm font-medium text-[#2884EF]"
           >
             Очистить
           </button>
-          <button @click="toggleFilter" class="text-sm font-medium leading-[14px] text-[#A1A5B7]">
+          <button
+            @click="toggleFilter"
+            class="flex items-center gap-1 text-sm font-medium text-[#A1A5B7]"
+          >
             {{ showFilters && selectedFilters.length === 0 ? 'Скрыть фильтр' : 'Фильтр' }}
             <img
-              :class="{ rotate: showFilters }"
-              class="inline-block"
+              :class="{ 'rotate-180': showFilters }"
               src="/svg/arrow.svg"
               width="10px"
               height="6px"
@@ -154,11 +121,11 @@ const inputField = ref<HTMLInputElement | null>(null)
           :key="filter"
           @click="toggleFilterSelection(filter)"
           :class="{ active: isActiveFilter(filter) }"
-          @mouseenter="showCrossIcon(filter)"
-          @mouseleave="resetIcon(filter)"
+          @mouseenter="updateIcon(filter)"
+          @mouseleave="updateIcon(filter)"
         >
           {{ filter }}
-          <img :src="getIcon(filter)" class="block max-sm:h-[9.3px] max-sm:w-[9.3px]" />
+          <img :src="currentIcons[filter]" />
         </button>
       </div>
     </div>
@@ -166,7 +133,6 @@ const inputField = ref<HTMLInputElement | null>(null)
 </template>
 
 <style scoped lang="sass">
-
 .input-filter
   &:hover
     border: 1px solid #3E97FF
@@ -178,22 +144,14 @@ const inputField = ref<HTMLInputElement | null>(null)
     font-weight: 500
     color: #D8D8E5
 
-.rotate
-  transform: rotate(180deg)
-
 .filter-buttons
   padding-top: 20px
   display: flex
   gap: 8px
   flex-wrap: wrap
 
-  @media (max-width: 425px) and (min-width: 375px)
-    padding-top: 12px
-    padding-bottom: 12px
-    margin-top: 62px
-
-  @media (max-width: 375px)
-    margin-top: 35px
+  @media (max-width: 425px)
+    padding-top: 62px
 
   button
     display: flex
@@ -206,13 +164,7 @@ const inputField = ref<HTMLInputElement | null>(null)
     font-size: 16px
     line-height: 16px
     font-weight: 500
-    transition: background-color 0.2s ease
-
-    @media (max-width: 425px) and (min-width: 375px)
-      font-size: 14px
-      line-height: 14px
-      padding: 7px 14px
-      padding-bottom: 7px
+    transition: background-color 0.5s ease
 
     &:hover
       background-color: #3E97FF33
@@ -221,14 +173,11 @@ const inputField = ref<HTMLInputElement | null>(null)
       background-color:  #3E97FF
       color: #FFFFFF
 
-// Классы для адаптивной верстки с контрольными точками
-.app-filter-container
-  width: 100%
-
+// adaptive classes
 .app-filter-wrapper
-
   padding-left: 95px
   padding-right: 95px
+
   @media (max-width: 800px) and (min-width: 425px)
     padding-left: 30px
     padding-right: 30px
@@ -246,17 +195,9 @@ const inputField = ref<HTMLInputElement | null>(null)
     position: relative
     min-height: 98px
 
-.app-filter-header
-  display: flex
-  align-items: center
-  gap: 40px
-  @media (max-width: 425px)
-
 .app-filter-title
   font-size: 32px
-  @media (max-width: 1400px) and (min-width: 425px)
-    font-size: 32px
-  @media (max-width: 425px) and (min-width: 375px)
+  @media (max-width: 431px) and (min-width: 375px)
     font-size: 24px
     line-height: 24px
   @media (max-width: 375px)
